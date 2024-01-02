965e774cec426ebf21d33c1bf0eb66ae1d71cc10
Fraoucene Fares <fares.fraoucene@laplateforme.io>
2024-01-02 22:54:48 +0100

    changement job7

--- Job08.py
+++ Job08.py
@@ -1,46 +1,26 @@
 import math
 class Cercle():
-    def __init__(self) -> None:
-        self.rayon1 = 4
-        self.rayon2 = 7
-    def changerRayonCercle1(self, new_value):
-        self.rayon1 = new_value
-    def changerRayonCercle2(self, new_value):
-        self.rayon2 = new_value
-    def circonference1(self):
-        circonference1 = 2 * math.pi * self.rayon1
+    def __init__(self, rayon) :
+        self.rayon = rayon
+    def changerRayonCercle(self, new_value):
+        self.rayon = new_value
+    def circonference(self):
+        circonference1 = 2 * math.pi * self.rayon
         return circonference1
-    def circonference2(self):
-        circonference2 = 2 * math.pi * self.rayon2
-        return circonference2
-    def air1(self):
-        air1 = math.pi * (self.rayon1**2)
+    def air(self):
+        air1 = math.pi * (self.rayon**2)
         return air1
-    def air2(self):
-        air2 = math.pi * (self.rayon2**2)
-        return air2
-    def diametre1(self):
-        diametre1 = self.rayon1 * 2
+    def diametre(self):
+        diametre1 = self.rayon * 2
         return diametre1
-    def diametre2(self):
-        diametre2 = self.rayon2 * 2
-        return diametre2
-    def afficherInfos1(self):
-        print(f"Info du cercle 2(rayon de {self.rayon1}) :")
-        print("Circonference: ", self.circonference1())
-        print("Aire: ", self.air1())
-        print("Diametre: ", self.diametre1())
-    def afficherInfos2(self):
-        print(f"Info du cercle 2(rayon de {self.rayon2}) :")
-        print("Circonference: ", self.circonference2())
-        print("Aire: ", self.air2())
-        print("Diametre: ", self.diametre2())   
+    def afficherInfos(self):
+        print(f"Info du cercle 2(rayon de {self.rayon}) :")
+        print("Circonference: ", self.circonference())
+        print("Aire: ", self.air())
+        print("Diametre: ", self.diametre())
+  
 
-n = Cercle()
-
-n.afficherInfos1()
-n.afficherInfos2()
-n.changerRayonCercle1(10)
-n.changerRayonCercle2(20)
-n.afficherInfos1()
-n.afficherInfos2()
+n = Cercle(4)
+n.afficherInfos()
+n.changerRayonCercle(7)
+n.afficherInfos()
--- Job09.py
+++ Job09.py
@@ -4,10 +4,12 @@ class Produit():
         self.prixHT = prixHT
         self.TVA = TVA
         self.prixTVA = self.CalculerPrixTTC()
+
     def CalculerPrixTTC(self):
         TVA = self.prixHT * (self.TVA / 100)
         result = self.prixHT + TVA
         return result
+    
     def afficher(self):
         return [self.nom,self.prixHT,self.TVA, self.prixTVA]
     def getNom(self):
@@ -18,6 +20,7 @@ class Produit():
         return self.TVA
     def getPrixTTC(self):
         return self.prixTVA
+    
     def setNom(self, nom):
         self.nom = nom
     def setPrixHT(self, prixHT):
@@ -28,7 +31,9 @@ class Produit():
         self.updatePrixTTC()
     def updatePrixTTC(self):
         self.prixTTC = self.CalculerPrixTTC()
+
 coca = Produit("Coca-Cola", 1,50)
 print(f"Nom : {coca.getNom()} / ",f"PrixHT : {coca.getPrixHT()}€ / ",f"PrixTTC : {coca.getPrixTTC()}€")
+
 beignet = Produit("Beignet", 2,70)
 print(f"Nom : {beignet.getNom()} / ",f"PrixHT : {beignet.getPrixHT()}€ / ",f"PrixTTC : {beignet.getPrixTTC()}€")
